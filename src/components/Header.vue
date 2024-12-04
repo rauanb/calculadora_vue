709852153f55ed163cb2874c3e7b7affc727bb2f
rauanb <rauan_b@hotmail.com>
2024-12-04 06:10:18 -0300

    Header formatado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-    <h1>Calculadora</h1>
+    <header class="p-5 my-4 bg-light rounded-3 text-center">
+        <h1 class="fw-bold">Calculadora</h1>
+    </header>
 </template>
 
 <style scoped>
-    h1 {
+    /* h1 {
         background-color: yellow;
-    }
+    } */
 </style>
