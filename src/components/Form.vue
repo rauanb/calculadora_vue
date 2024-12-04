c554ceae9bd2ca15dd4329452e45e732e372e81c
rauanb <rauan_b@hotmail.com>
2024-12-04 06:27:44 -0300

    Formulario formatado

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,10 +1,26 @@
 <template>
-    <p>Formulario</p>
-    <form action=""></form>
+    <form>
+        <div class="row">
+            <div class="col">
+                <input class="form-control" required type="number" placeholder="Número 1">
+            </div>
+            <div class="col">
+                <input class="form-control" required type="number" placeholder="Número 2">
+            </div>
+            <div class="col">
+                <select class="form-control">
+                    <option value="Soma">Soma</option>
+                    <option value="Subtracao">Subtração</option>
+                    <option value="Multiplicacao">Multiplicação</option>
+                    <option value="Divisao">Divisão</option>
+                </select>
+            </div>
+        </div>
+    </form>
 </template>
 
 <style scoped>
-    p {
+    /* p {
         background-color: aqua;
-    }
+    } */
 </style>
