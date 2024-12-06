2fcba30004f0acfa8a0b4b2910018642f584b95d
rauanb <rauan_b@hotmail.com>
2024-12-06 05:23:58 -0300

    Footer fixado

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,18 @@
 <template>
-    <p>
-        Feito com carinho pelo <a href="https://www.linkedin.com/in/rauan-liberato/">Rauan</a>
-    </p>
+    <footer class="m-0 p4">
+        <p class="text-center bg-light m-0 p-3">
+            Feito com carinho pelo <a href="https://www.linkedin.com/in/rauan-liberato/">Rauan</a>
+        </p>
+    </footer>
 </template>
 
 <style scoped>
-    p {
-        background-color: yellow;
+    footer {
+        /* background-color: yellow; */
+        position: fixed;
+        width: 100%;
+        left: 0;
+        bottom: 0;
+        /* height: 32px; */
     }
 </style>
